7fb358a6778f0c09f561c3dcc14bd865d257fb34
RaenonX <raenonx0710@gmail.com>
2025-10-22 03:45:11 +0800

    Updated RP model cache handling
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- api.py
+++ api.py
@@ -52,6 +52,7 @@ def print_final_results(entries: list[tuple[bool, OptimizerSolvedDataEntry]]):
             print_final_result_entry(entry)
 
 
+# The return `bool` indicates whether the result was cached or recalculated.
 def use_or_refresh_solved_pokemon(
     pokemon_name: str,
     data_of_pokemon: DataFrame,
@@ -71,13 +72,13 @@ def use_or_refresh_solved_pokemon(
     is_valid_store = store.is_valid()
     is_last_fit_passed = is_last_fit_perfect(pokemon_name, data_of_pokemon, last_fit, x0, unpack_info)
 
-    if not is_valid_store:
-        print(f"Recalculating {pokemon_name:25}: Invalid store")
+    if is_last_fit_passed:
+        if is_valid_store:
+            print(f"No data update for {pokemon_name:25}, but the last fit was not perfect")
+        else:
+            print(f"Recalculating {pokemon_name:25}: Data updated while last fit failed")
 
-    if not is_last_fit_passed:
-        print(f"Recalculating {pokemon_name:25}: Last fit failed")
-
-    if is_valid_store and is_last_fit_passed:
+    if is_valid_store or is_last_fit_passed:
         return True, store.data()
 
     solved_data = process_pokemon(
